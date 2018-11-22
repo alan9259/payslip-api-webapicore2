74e758821ae41583449e9d4d04b16d9ae5dc312f
Alan <32184821+alan9259@users.noreply.github.com>
2018-11-22 14:02:23 +1300

    Add files via upload

---
+++ PaySlip/LoggerService/LoggerService.cs
@@ -0,0 +1,33 @@
+﻿using NLog;
+
+namespace PaySlip.LoggerService
+{
+    public interface ILoggerService
+    {
+        void Info(string message);
+        void Debug(string message);
+        void Error(string message);
+    }
+
+    public class LoggerService : ILoggerService
+    {
+        private readonly Logger _logger;
+
+        public LoggerService()
+        {
+            _logger = LogManager.GetCurrentClassLogger();
+        }
+        public void Info(string message)
+        {
+            _logger.Info(message);
+        }
+        public void Debug(string message)
+        {
+            _logger.Debug(message);
+        }
+        public void Error(string message)
+        {
+            _logger.Error(message);
+        }
+    }
+}
